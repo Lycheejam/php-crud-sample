b43931244dd7ceb6a8d1a6e80bd5304233053837
Lycheejam <lycheejamwow@gmail.com>
2019-06-10 22:29:28 +0900

    add comment

--- src/static/js/string_length.js
+++ src/static/js/string_length.js
@@ -1,3 +1,8 @@
+/**
+ * 文字列カウンター
+ * @param {string} countTarget カウント対象のinput要素ID名
+ * @param {string} resultTarget 文字列カウント結果の挿入先ID名
+ */
 function StringCount (countTarget, resultTarget) {
   var len = document.getElementById(countTarget).value.length;
   document.getElementById(resultTarget).innerText = len;
