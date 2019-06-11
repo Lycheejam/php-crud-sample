6a1670a1b149e4176c88a6c875cc9e39094d379f
Lycheejam <lycheejamwow@gmail.com>
2019-06-11 22:48:12 +0900

    やっていき

--- src/static/js/string_length.js
+++ src/static/js/string_length.js
@@ -1,9 +1,13 @@
+if (typeof Utils === 'undefined') {
+  var Utils = {};
+};
+
 /**
  * 文字列カウンター
  * @param {string} countTarget カウント対象のinput要素ID名
  * @param {string} resultTarget 文字列カウント結果の挿入先ID名
  */
-function StringCount (countTarget, resultTarget) {
+Utils.StringCount = function (countTarget, resultTarget) {
   var len = document.getElementById(countTarget).value.length;
   document.getElementById(resultTarget).innerText = len;
 }
