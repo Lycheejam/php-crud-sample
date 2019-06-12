34dccb6d247dd0c04d28b623d2b881f4ff129215
Lycheejam <lycheejamwow@gmail.com>
2019-06-12 23:47:59 +0900

    add count ex function

--- src/static/js/string_length.js
+++ src/static/js/string_length.js
@@ -7,7 +7,28 @@ if (typeof Utils === 'undefined') {
  * @param {string} countTarget カウント対象のinput要素ID名
  * @param {string} resultTarget 文字列カウント結果の挿入先ID名
  */
-Utils.StringCount = function (countTarget, resultTarget) {
+Utils.StringCount = function(countTarget, resultTarget) {
   var len = document.getElementById(countTarget).value.length;
   document.getElementById(resultTarget).innerText = len;
+}
+
+Utils.StringCountEx = function(countTarget, resultTarget, maxCount = 0) {
+  let text = document.getElementById(countTarget);
+  let result = document.getElementById(resultTarget);
+
+  let len = text.value.length;
+
+  let color = '#000000';
+  let bcolor = '#ffffff';
+
+  if(maxCount > 0 && len > maxCount) {
+    len = maxCount - len;
+    color = '#ff0000';
+    bcolor = '#ff0000';
+  }
+
+  result.style.color = color;
+  text.style.backgroundColor = bcolor;
+
+  result.innerText = len;
 }
